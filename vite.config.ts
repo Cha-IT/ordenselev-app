cd7b5025854cb7a8a37674a52d8a6ea7259c96c5
Finn <finng2008@outlook.com>
2026-01-04 14:26:56 +0100

    Added docker-compose.yml file with rerouting for api via one port.

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    fs: {
-      strict: true,
-      allow: [
-        resolve(__dirname, 'src/client'),
-        resolve(__dirname, 'node_modules'),
-        resolve(__dirname, 'public'),
-        resolve(__dirname, 'index.html'),
-        resolve(__dirname, 'history.html'),
-      ],
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        history: resolve(__dirname, 'history.html'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        }
+      },
+      fs: {
+        strict: true,
+        allow: [
+          resolve(__dirname, 'src/client'),
+          resolve(__dirname, 'node_modules'),
+          resolve(__dirname, 'public'),
+          resolve(__dirname, 'index.html'),
+          resolve(__dirname, 'history.html'),
+        ],
       },
     },
-  },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          history: resolve(__dirname, 'history.html'),
+        },
+      },
+    },
+  }
 })
