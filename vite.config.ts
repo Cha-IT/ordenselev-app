82725145c1b57f922cc2962f1b98721b7d0f345c
FinnakaLite <Finng2008@outlook.com>
2026-01-08 10:16:14 +0100

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: true,
       proxy: {
         '/api': {
-          target: 'http://localhost:3000',
+          target: 'http://api:3000',
           changeOrigin: true,
         }
       },
